7602ae1c337f1688f83eb4e94e6649e382d5834b
mintyry <gitmintyry@gmail.com>
2023-10-26 15:28:36 -0700

    required shapes file, wrote if statements, able to now writeFile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const choices = require('./lib/shapes.js')
+const { Triangle, Circle, Square } = require('./lib/shapes.js')
 
 const questions = [
     {
@@ -32,10 +32,31 @@ function start() {
         .prompt(questions)
         .then(function (data) {
             console.log(data)
-            if (data.shapeChoice === 'circle'){
-                console.log(choices.Circle);
+            let finalSVG =''
+
+            if (data.shapeChoice === 'Triangle') {
+                console.log(Triangle);
+                //why is this logging the class in an array? Is it bc that's what the user's answer translates to in inquirer?
+                const userTriangle = new Triangle(data.colorChoice, data.logoName, data.textColorChoice);
+                console.log(userTriangle.render());
+                finalSVG = userTriangle.render();
+
+            } else if (data.shapeChoice === 'Circle') {
+                console.log(Circle);
+                //why is this logging as an array of the class?
+                const userCircle = new Circle(data.colorChoice, data.logoName, data.textColorChoice);
+                console.log(userCircle.render());
+                finalSVG = userCircle.render();
+
+            } else if (data.shapeChoice === 'Square') {
+                console.log(Square);
+                //why is this logging as an array of the class?
+                const userSquare = new Square(data.colorChoice, data.logoName, data.textColorChoice);
+                console.log(userSquare.render());
+                finalSVG = userSquare.render();
+
             }
-            fs.writeFile('./dist/logo.svg', choices, (err) => {
+            fs.writeFile('./dist/logo.svg', finalSVG , (err) => {
                 err ? console.log(err) : console.log('Generated logo.svg');
             })
         })
