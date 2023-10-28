3e97e93c5491045311acf045ce120d49bffc585a
mintyry <gitmintyry@gmail.com>
2023-10-27 23:26:16 -0700

    commented index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
+// requiring necessary modules and shapes classes to access template of shapes to render from user's answers
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./lib/shapes.js')
 
+// validate function to check that user's logo text input is not longer than three characters
 const validateAnswer = (answer) => {
     return answer.length > 3 ? "Logo cannot be more than 3 characters long." : true;
   };
 
+// validate color input using regex -- checks that it is either a string of letters or a hexadecimal number
 const validateColor = (userColor) => {
     const colorRegex = /^([a-zA-Z]+|\#[0-9A-Fa-f]{6})$/;
     if(colorRegex.test(userColor)) {
         return true;
     } else {
         return 'Please enter a valid color.';
-    }
-    
+    } 
 }
 
+// quetions that prompts user's answers to generate logo
 const questions = [
     {
         type: 'input',
@@ -43,38 +46,39 @@ const questions = [
     }
 ];
 
+// function that holds what will happen once app starts
 function start() {
     inquirer
+        // prompt will proceed with the above questions
         .prompt(questions)
         .then(function (data) {
+            // depending on the shape user chooses, the rest of choices will be subsequently passed through to render the logo.
             let finalSVG = '';
-            console.log(data)
+         
             if (data.shapeChoice === 'Triangle') {
-                console.log(Triangle);
-                //why is this logging the class in an array? Is it bc that's what the user's answer translates to in inquirer?
+
                 const userTriangle = new Triangle(data.logoName, data.textColorChoice, data.colorChoice);
-                console.log(userTriangle.render());
                 finalSVG = userTriangle.render();
 
             } else if (data.shapeChoice === 'Circle') {
-                console.log(Circle);
-                //why is this logging as an array of the class?
+       
                 const userCircle = new Circle(data.logoName, data.textColorChoice, data.colorChoice);
-                console.log(userCircle.render());
                 finalSVG = userCircle.render();
 
             } else if (data.shapeChoice === 'Square') {
-                console.log(Square);
-                //why is this logging as an array of the class?
+           
                 const userSquare = new Square(data.logoName, data.textColorChoice, data.colorChoice);
-                console.log(userSquare.render());
                 finalSVG = userSquare.render();
 
             }
+
+            // we write whatever finalSVG becomes into the logo.svg file; if successful,
+            // generated logo.svg message will display in command line.
             fs.writeFile('./dist/logo.svg', finalSVG, (err) => {
                 err ? console.log(err) : console.log('Generated logo.svg');
             })
         })
 }
 
+// call function to start when user invokes app
 start();
